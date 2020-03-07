7fff0bff74fe0d2109eb2c6bbc1e9633bf00ca47
Marcoru97 <marco.russi97@gmail.com>
2020-03-07 23:59:02 +0000

    Add sqlite3 support

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,16 +1,31 @@
 import { Router } from "express";
 import si from 'systeminformation';
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
 
 const router = Router();
 
+
+class User extends Model {}
+
 router.get('/', (req, res, next) =>  {
 
-  si.cpuCache()
-      .then(data => {
-        res.status(200).send(data);
-        next();
-      })
-      .catch(error => console.error(error));
+  const sequelize = new Sequelize('sqlite::memory:');
+  
+  User.init({
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE
+  }, { sequelize, modelName: 'user' });
+  
+  sequelize.sync()
+    .then(() => User.create({
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20)
+    }))
+    .then(jane => {
+      res.status(200).send(jane.toJSON());
+      next();
+    });
 });
 
 
