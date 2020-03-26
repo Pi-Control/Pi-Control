7726b4b86e688cd297f2fa96d02c3fca2095972f
Marcoru97 <marco.russi97@gmail.com>
2020-03-26 23:17:01 +0000

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import graphqlHTTP from 'express-graphql';
 import { json } from 'body-parser';
+import cors from 'cors';
 import expressPlayground from 'graphql-playground-middleware-express';
 
 import * as Bootstrap from './bootstrap';
@@ -14,6 +15,7 @@ import { schema, resolvers } from './graphql';
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.use(
   '/graphql',
