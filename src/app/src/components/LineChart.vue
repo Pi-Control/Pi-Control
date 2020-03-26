cd31603ef8b566e45839dfaa727fbac25904a220
Marco Rußinski <marco.russinski@bitburst.net>
2020-03-27 00:43:58 +0100

    Add dummy chart data

--- src/app/src/components/LineChart.vue
+++ src/app/src/components/LineChart.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="small">
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
+  <div>
+    <div class="small">
+      <line-chart :chart-data="memoryChartData" :options="options"></line-chart>
+    </div>
+    <div class="small">
+      <line-chart :chart-data="cpuChartData" :options="options"></line-chart>
+    </div>
   </div>
 </template>
 
@@ -14,32 +18,118 @@ export default {
   },
   data() {
     return {
-      datacollection: null,
+      cpuChartData: null,
+      memoryChartData: null,
+
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        elements: {
+          line: {
+            tension: 0,
+          },
+          point: {
+            radius: 0,
+            hitRadius: 10,
+            hoverRadius: 0,
+          },
+        },
+      },
     };
   },
   mounted() {
-    this.fillData();
+    this.fetchData();
+
+    setInterval(() => {
+      this.fetchData();
+    }, 5000);
   },
+
   methods: {
-    fillData() {
-      this.datacollection = {
-        labels: [this.getRandomInt(), this.getRandomInt()],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [this.getRandomInt(), this.getRandomInt()],
-          },
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [this.getRandomInt(), this.getRandomInt()],
-          },
-        ],
-      };
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    fetchData() {
+      const dice = 3;
+      const sides = 6;
+      const query = `
+query {
+  metrics {
+    memory {type value timestamp}
+    cpu {type value timestamp}
+  }
+}`;
+
+      fetch('http://192.168.2.58:3000/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          query,
+          variables: { dice, sides },
+        }),
+      })
+        .then(r => r.json())
+        .then(data => {
+          const memoryData = data.data.metrics.memory;
+          const cpuData = data.data.metrics.cpu.filter(
+            cpu => cpu.type === 'cpu_temperature'
+          );
+
+          const cpuSpeed = data.data.metrics.cpu.filter(
+            cpu => cpu.type === 'cpu_speed'
+          );
+
+          this.memoryChartData = {
+            labels: memoryData.map(mem => {
+              const date = new Date(parseInt(mem.timestamp, 10));
+              return (
+                date.getHours() +
+                ':' +
+                date.getMinutes() +
+                ':' +
+                date.getSeconds()
+              );
+            }),
+            datasets: [
+              {
+                label: 'Memory Usage',
+                borderColor: '#d32f2f',
+                backgroundColor: 'rgba(211,47,47, 0.3)',
+                data: memoryData.map(mem =>
+                  (mem.value / 1000 / 1000).toFixed(2)
+                ),
+              },
+            ],
+          };
+
+          this.cpuChartData = {
+            labels: cpuData.map(mem => {
+              const date = new Date(parseInt(mem.timestamp, 10));
+              return (
+                date.getHours() +
+                ':' +
+                date.getMinutes() +
+                ':' +
+                date.getSeconds()
+              );
+            }),
+            datasets: [
+              {
+                label: 'CPU Speed',
+                borderColor: '#303F9F',
+                backgroundColor: 'rgba(48,63,159, 0.3)',
+                data: cpuSpeed.map(mem => mem.value),
+              },
+              {
+                label: 'CPU Temperature',
+                borderColor: '#d32f2f',
+                backgroundColor: 'rgba(211,47,47, 0.3)',
+                data: cpuData.map(mem => mem.value),
+              },
+            ],
+          };
+        });
     },
   },
 };
@@ -47,7 +137,7 @@ export default {
 
 <style>
 .small {
-  max-width: 600px;
-  margin: 150px auto;
+  max-width: 80%;
+  margin: 50px auto;
 }
 </style>
