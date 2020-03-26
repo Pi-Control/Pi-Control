5f82f7858098c255d93e09b2e6ec010eb3463077
Marco Rußinski <marco.russinski@bitburst.net>
2020-03-26 23:30:25 +0100

    Add chartjs and dummy chart

---
+++ src/app/src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard">
+    <line-chart />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import LineChart from '@/components/LineChart.vue';
+
+@Component({
+  components: {
+    LineChart,
+  },
+})
+export default class Dashboard extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
