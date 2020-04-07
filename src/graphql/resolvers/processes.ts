b8c87a88f6c1e7213c9e8860067883f64c6c9c38
Marcoru97 <marco.russi97@gmail.com>
2020-04-07 22:12:32 +0100

    Add kill process mutation

--- src/graphql/resolvers/processes.ts
+++ src/graphql/resolvers/processes.ts
@@ -1,5 +1,18 @@
 import { processes, Systeminformation } from 'systeminformation';
+import { exec } from 'child_process';
 
 export default {
+  killProcess: ({ pid }: { pid: number }): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+      // TODO: Move to lib
+      exec(`kill ${pid}`, (error, stdout, stderr) => {
+        if (error) {
+          reject(stderr);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  },
   processes: async (): Promise<Systeminformation.ProcessesData> => processes(),
 };
