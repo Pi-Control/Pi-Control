5eca70ba1cae5831a0a2109d0646dfebbdcfa7a3
Marcoru97 <marco.russi97@gmail.com>
2020-04-09 21:50:09 +0100

    Fix graphql metrics

--- src/graphql/resolvers/metrics.ts
+++ src/graphql/resolvers/metrics.ts
@@ -3,15 +3,11 @@ import MemoryMetrics from '../../models/MemoryMetrics';
 
 export default {
   Query: {
-    metrics: (): {} => ({ cpu: null, memory: null }),
-  },
-
-  MetricsCollection: {
-    cpu(): Promise<CpuMetrics[]> {
+    metricsCpu(): Promise<CpuMetrics[]> {
       return CpuMetrics.findAll();
     },
 
-    memory(): Promise<MemoryMetrics[]> {
+    metricsMemory(): Promise<MemoryMetrics[]> {
       return MemoryMetrics.findAll();
     },
   },
